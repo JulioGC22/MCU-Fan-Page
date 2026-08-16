a8e2562092e364ee343210db3ba812fbeeb75ed0
Julio G <juliogamez@Julios-MacBook-Pro.local>
2026-08-15 21:44:13 -0700

    Data: questions array of 15 objects are added

---
+++ js/quiz.js
@@ -0,0 +1,111 @@
+
+// Questions with needed properties
+
+const q1 = {
+  question: "In Iron Man (2008), what is the name of the terrorist organization that kidnaps Tony Stark?",
+  choices: ["The Ten Rings", "HYDRA", "A.I.M.", "The Hand"],
+  correctAns: "The Ten Rings",
+  category: "Phase 1"
+};
+
+const q2 = {
+  question: "What is the name of Thor's enchanted hammer?",
+  choices: ["Stormbreaker", "Mjolnir", "Gungnir", "Johnathan"],
+  correctAns: "Mjolnir",
+  category: "Phase 1"
+};
+
+const q3 = {
+  question: "Which Infinity Stone is contained within Loki's scepter?",
+  choices: ["Space Stone", "Power Stone", "Mind Stone", "Reality Stone"],
+  correctAns: "Mind Stone",
+  category: "Phase 1"
+};
+
+const q4 = {
+  question: "What is the name of Peter Quill's spaceship?",
+  choices: ["Benator", "Milano", "Dark Aster", "Sanctuary II"],
+  correctAns: "Milano",
+  category: "Phase 2"
+};
+
+const q5 = {
+  question: "Which organization has secretly infiltrated S.H.I.E.L.D.?",
+  choices: ["HYDRA", "The Ten Rings", "A.I.M", "The Ravegers"],
+  correctAns: "HYDRA",
+  category: "Phase 2"
+};
+
+const q6 = {
+  question: "What is the name of the artificial intelligence that becomes Vision?",
+  choices: ["JARVIS", "FRIDAY", "Ultrn", "EDITH"],
+  correctAns: "JARVIS",
+  category: "Phase 2"
+};
+
+const q7 = {
+  question: "What agreement divides the Avengers over whether superheroes should be regulated",
+  choices: ["The Sokovia Accords", "The Geneva Protocols", "The Vienna Accords", "The Stark Initiative" ],
+  correctAns: "The Sokovia Accords",
+  category: "Phase 3"
+};
+
+const q8 = {
+  question: "Which dimension is ruled by Dormammu?",
+  choices: ["The Quantum Realm", "The Mirror Dimension", "The Astral Plane", "The Dark Dimension"],
+  correctAns: "The Dark Dimension",
+  category: "Phase 3"
+};
+
+const q9 = {
+  question: "What is the fictional African nation ruled by T'Challa?",
+  choices: ["Genosha", "Wakanda", "Latveria", "Sokovia"],
+  correctAns: "Wakanda",
+  category: "Phase 3"
+};
+
+const q10 = {
+  question: "Which Infinity Stone is located on Vision's forehead?",
+  choices: ["Mind Stone", "Soul Stone", "Time Stone", "Reality Stone"],
+  correctAns: "Mind Stone",
+  category: "Phase 3"
+};
+
+const q11 = {
+  question: "What is the name of the spell Doctor Strange attempts to cast to make everyone forget Peter Parker's identity?",
+  choices: ["The Memory Spell", "The Runes of Kamar-Taj","The Forgetting Spell", "The Runes of Kof-Kol"],
+  correctAns: "The Runes of Kof-Kol",
+  category: "Phase 4"
+};
+
+const q12 = {
+  question: "What is the name of Shang-Chi's sister?",
+  choices: ["Ying Nan", "Xialing", "Ying Li", "Xu Wenwu"],
+  correctAns: "Xialing",
+  category: "Phase 4"
+};
+
+const q13 = {
+  question: "Who is revealed to be the main antagonist in Doctor Strange in the Multiverse of Madness?",
+  choices: ["Wanda Maximoff", "Christine Palmer", "America Chavez", "Clea"],
+  correctAns: "Wanda Maximoff",
+  category: "Phase 4"
+};
+
+const q14 = {
+  question: "What is the name of the villain who created Rocket Raccon?",
+  choices: ["Adam Warlock", "The High Evolutionary", "Ego", "Ronan"],
+  correctAns: "The High Evolutionary",
+  category: "Phase 5"
+};
+
+const q15 = {
+  question: "Which organization deals with timelines and alternate universes?",
+  choices: ["S.H.I.E.L.D", "The TVA", "Damage Control", "S.W.O.R.D"],
+  correctAns: "The TVA",
+  category: "Phase 5"
+};
+
+//Combining all question objects into one array
+
+const questions = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15];
